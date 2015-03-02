9378da22faba398d0ad6face91580fb3e9117b85
unknown <pperboires@DEV-DEXTER-02.AIDEV.BIZ>
2015-03-02 15:19:31 -0300

    Web Host configuration.

--- src/PingPong.WebHost/Program.cs
+++ src/PingPong.WebHost/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
 
 namespace PingPong.WebHost
 {
@@ -10,6 +11,13 @@ namespace PingPong.WebHost
     {
         static void Main(string[] args)
         {
+            const string url = "http://localhost:9999";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("\n\nAll Applications...");
+                Console.WriteLine("\n\nServer listening at {0}. Press enter to stop", url);
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ src/PingPong.WebHost/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LightInject;
+using Owin;
+using PingPong.Shared;
+
+namespace PingPong.WebHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var container = CreateInjectorContainer();
+            
+            app.Map("/ping", map => container.GetInstance<IModuleEngine>("ping").RegisterApi(map));
+            app.Map("/pong", map => container.GetInstance<IModuleEngine>("pong").RegisterApi(map));
+        }
+
+        private static ServiceContainer CreateInjectorContainer()
+        {
+            var container = new ServiceContainer();
+            container.RegisterInstance(Configure());
+            container.Register<IModuleEngine, Ping.Engine>("ping");
+            container.Register<IModuleEngine, Pong.Engine>("pong");
+
+            return container;
+        }
+
+        private static IModuleConfiguration Configure()
+        {
+            var tenantConfigurator = new TenantConfigurator();
+            tenantConfigurator.RegisterTenant("default", "Server=.;Database=pingpong;Trusted_Connection=True;");
+
+            return new MemoryConfiguration(tenantConfigurator, "amqp://jf3l1x:password@localhost:5672/testes");
+        }
+    }
+}
